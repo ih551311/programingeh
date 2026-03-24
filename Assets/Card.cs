aa65edcfa407d7f15403e8d791bcdd5338f07c58
ih551311 <ih551311@gmail.com>
2026-03-24 17:57:57 +0900

    카드 코딩 만들기

---
+++ Assets/Card.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+// 변수
+//정보
+
+//함수
+
+public class Card : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+       text.text = Random.Range(0,10).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Rotate(0, 2, 0); 
+    }
+}
